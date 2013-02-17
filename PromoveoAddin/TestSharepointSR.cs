8c2db8890ec1ed528129716cf42862db49db7040
Elias Balmer <elias.balmer@bluewin.ch>
2013-02-17 17:24:10 +0100

    with sp service reference

---
+++ PromoveoAddin/TestSharepointSR.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using PromoveoAddin.SharepointServiceReference;
+
+namespace PromoveoAddin
+{
+    public class TestSharepointSR
+    {
+        public TestSharepointSR()
+        {
+            GetWebPartPageRequest rr = new GetWebPartPageRequest();
+            
+        }
+
+
+    }
+}
