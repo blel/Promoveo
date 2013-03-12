ff223d77b5af0d114f1d1cff88d844e772370a77
Elias Balmer <elias.balmer@bluewin.ch>
2013-03-12 08:07:35 +0100

    link merger

--- PromoveoAddin/ModelMerger.cs
+++ PromoveoAddin/ModelMerger.cs
@@ -40,6 +40,7 @@ namespace PromoveoAddin
                 Merge(fileName);
             }
             _resultDoc.Pages[_initialPageToRemove.Name].Delete(Convert.ToInt16(false));
+            LocalizeVisioHyperlinks();
         }
 
         private void Merge(string fileName)
@@ -55,12 +56,79 @@ namespace PromoveoAddin
 
         private void MergePage(Visio.Page pageToMerge)
         {
-            PagePreparer preparer = new PagePreparer(_resultDoc.Pages.Add(), pageToMerge);
+            Visio.Page resultPage = _resultDoc.Pages.Add();
+            PagePreparer preparer = new PagePreparer(resultPage, pageToMerge);
             preparer.CopyFormatToDestinationPage();
             preparer.CopyPage();
         }
 
 
+        private List<Visio.Hyperlink> GetHyperlinks(Visio.Page page)
+        {
+            List<Visio.Hyperlink> resultList = new List<Visio.Hyperlink>();
+            foreach (Visio.Shape shape in page.Shapes)
+            {
+                resultList.AddRange(shape.Hyperlinks.Cast<Visio.Hyperlink>().ToList());
+            }
+            return resultList;
+        }
+
+        private void LocalizeVisioHyperlinks()
+        {
+            foreach (Visio.Page page in _resultDoc.Pages)
+            {
+                foreach (Visio.Hyperlink link in GetHyperlinks(page))
+                {
+                    if (IsVisioFile(link.Address))
+                    {
+                        if (string.IsNullOrEmpty(link.SubAddress))
+                        {
+                            string FirstPageNameOfDoc = GetFirstPageNameOfDoc(link.Address);
+                            if (IsPageAvailable(FirstPageNameOfDoc))
+                                link.SubAddress = FirstPageNameOfDoc;
+                            else
+                                MessageBox.Show("The Link Target is not available.");
+
+                        }
+                        else
+                        {
+                            if (IsPageAvailable(link.SubAddress))
+                            {
+                                link.Address = string.Empty;
+                            }
+                            else
+                            {
+                                MessageBox.Show("The Link Target is not available.");
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private bool IsVisioFile(string fileName)
+        {
+            List<string> VisioExtensions = new List<string>() { ".vsd" };
+            string extension = fileName.Substring(fileName.Length - 4, 4);
+            if (!string.IsNullOrEmpty(VisioExtensions.Find(cc => cc == extension)))
+                return true;
+            return false;
+        }
+
+        private string GetFirstPageNameOfDoc(string filename)
+        {
+            var document = _app.VisioApp.Documents.OpenEx(filename, (short)Visio.VisOpenSaveArgs.visOpenHidden + (short)Visio.VisOpenSaveArgs.visOpenDontList);
+            string pageName = document.Pages[1].Name;
+            document.Saved = true;
+            document.Close();
+            return pageName;
+        }
+
+        private bool IsPageAvailable(string pageName)
+        {
+            return _resultDoc.Pages.Cast<Visio.Page>().ToList().Find(cc =>cc.Name == pageName)!=null; 
+                  
+        }
 
 
     }
